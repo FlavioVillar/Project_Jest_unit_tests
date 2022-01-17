5035be15d9bc205d33c29c9a937f63819aebb9ed
FlavioVillar <flavio_villar@hotmail.com>
2022-01-17 16:16:34 -0300

    requisito 1 - finalizado

--- src/average.js
+++ src/average.js
@@ -13,9 +13,20 @@
 */
 
 const average = (array) => {
-  if (typeof array !== 'number' || array === ' ') {
+  if (array.length === 0) {
     return undefined;
   }
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
 };
+
+// let array = [2, 4, 5, 4, 3];
+// console.log(average(array));
 
 module.exports = average;
