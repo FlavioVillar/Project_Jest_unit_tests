33209e470b807bc0c6f8101a5e58354a2ea365ab
FlavioVillar <flavio_villar@hotmail.com>
2022-01-17 15:00:27 -0300

    requisito 1 - feito parte do undefined

--- src/average.js
+++ src/average.js
@@ -12,6 +12,10 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (typeof array !== 'number' || array === ' ') {
+    return undefined;
+  }
+};
 
 module.exports = average;
